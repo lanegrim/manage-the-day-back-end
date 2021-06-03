ea928a4b10ca654203cc2a8a194f383c0b3f273c
lanegrimaud <lanewgrimaud@gmail.com>
2021-06-03 09:19:55 -0400

    first routes working

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask import request
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/todos"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@app.route('/')
+def hello():
+    return {"hello": "world"}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+class TodosModel(db.Model):
+    __tablename__ = 'todos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String())
+    column = db.Column(db.String())
+    completed = db.Column(db.Boolean())
+
+    def __init__(self, task, column, completed):
+        self.task = task
+        self.column = column
+        self.completed = completed
+
+    def __repr__(self):
+        return f"< {self.task}>"
+
+
+@app.route('/todos', methods=['POST', 'GET'])
+def handle_todos():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_todo = TodosModel(
+                task=data['task'], column=data['column'], completed=data['completed'])
+            db.session.add(new_todo)
+            db.session.commit()
+            return {"message": f"task {new_todo.task} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+    elif request.method == 'GET':
+        todos = TodosModel.query.all()
+        results = [
+            {
+                "task": todo.task,
+                "column": todo.column,
+                "completed": todo.completed,
+            } for todo in todos]
+
+        return {"count": len(results), "todos": results}
